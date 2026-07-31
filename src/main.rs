b474fd23b6237255a93f4b8e2eb2b4c239701e83
Nitjsefn <>
2026-07-31 19:37:09 +0200

    added struct Task, and added func parse_string_to_tasks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::slice;
+use std::str::Split;
 use std::{collections::LinkedList, error::Error};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use chrono::{NaiveDateTime, Local};
+use chrono::{DateTime, NaiveDateTime, Local};
 
 fn main() -> Result<(), Box<dyn Error>>
 {
@@ -39,6 +41,28 @@ struct Task
 fn parse_string_to_tasks(s: &String) -> Result<LinkedList<Task>, Box<dyn Error>>
 {
     let mut tasks: LinkedList<Task> = LinkedList::new();
-
+    let mut split = s.split('\n');
+    let mut current_line_opt = split.next();
+    while(current_line_opt != None)
+    {
+        let current_line = current_line_opt.unwrap();
+        let splitted: Vec<&str> = current_line.split(";").collect();
+        let parse_err = "Cannot convert miliSec to NaiveDateTime";
+        let current_dt = DateTime::from_timestamp_millis(splitted[0].parse()?).ok_or(parse_err)?;
+        let expiration_dt = DateTime::from_timestamp_millis(splitted[1].parse()?).ok_or(parse_err)?;
+        let mut text = String::new();
+        for i in 2..splitted.len()
+        {
+            text = text + splitted[i];
+        }
+        let task = Task
+        {
+            creation_date: current_dt.naive_utc(),
+            expiration_date: expiration_dt.naive_utc(),
+            text: text
+        };
+        tasks.push_back(task);
+        current_line_opt = split.next();
+    }
     return Ok(tasks);
 }
